78f82bcfa175ed37e9e11872767d32d4e3b3a0fb
guido4f <james.fawke@g4pas.co.uk>
2017-03-03 14:39:53 +0000

    Limit to non-compressed Files

--- zebedee-reader/src/test/java/com/github/onsdigital/zebedee/reader/util/ContentExtractorTest.java
+++ zebedee-reader/src/test/java/com/github/onsdigital/zebedee/reader/util/ContentExtractorTest.java
@@ -15,7 +15,6 @@ import java.net.URI;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 
 /**
  * Created by fawks on 08/02/2017.
@@ -32,23 +31,7 @@ public class ContentExtractorTest {
         LOGGER.info("init([]) : rootFolder='{}'", ContentExtractorFactory.rootFolder);
     }
 
-    @Test
-    public void testExtractZip() throws Exception {
 
-
-        Dataset dataset = new Dataset();
-        dataset.setUri(new URI("ContentExtractorTest/dataset"));
-        DownloadSection ds = new DownloadSection();
-        ds.setFile("ContentExtractorTest.downloadFile.zip");
-        dataset.setDownloads(Lists.newArrayList(ds));
-        ContentExtractor instance = ContentExtractorFactory.getInstance(dataset,
-                                                                        "ContentExtractorTest.downloadFile.zip");
-        List<String> actual = instance.extract();
-
-        //Check the contents
-        assertNull(actual);
-
-    }
     @Test
     public void testExtractTxt() throws Exception {
 
@@ -73,21 +56,6 @@ public class ContentExtractorTest {
 
     }
 
-    @Test
-    public void testToLargeAFile() throws Exception {
 
-        Dataset dataset = new Dataset();
-        dataset.setUri(new URI("com/github/onsdigital/zebedee/util"));
-        DownloadSection ds = new DownloadSection();
-        dataset.setDownloads(Lists.newArrayList(ds));
-        ContentExtractor instance = ContentExtractorFactory.getInstance(dataset,
-                                                                        "com/github/onsdigital/zebedee/util/11MB.zip");
-        List<String> actual = instance.extract();
-        LOGGER.info("getInstance([]) : {}", actual);
-
-        //Check the contents
-        assertNull(actual);
-
-    }
 
 }
